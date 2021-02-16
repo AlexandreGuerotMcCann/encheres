006997edcd88247c2ba473fa4fed8a6a3bd579ad
SandrineZ3 <79086955+SandrineZ3@users.noreply.github.com>
2021-02-16 17:17:17 +0100

    couche BLL

---
+++ src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -0,0 +1,14 @@
+package fr.eni.encheres.bll;
+
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+	// Doit disposer d'un accès à la DAL en passant par le DAOFactory => Création de la DAOFactory dans DAL
+	private UtilisateurDAO daoUtilisateur;
+	
+	// Méthode pour obtenir un utilisateur de la BDD
+	public void ListeUtilisateur() {
+		daoUtilisateur = DAOFactory.getUtilisateurDAO();
+	}
+}
