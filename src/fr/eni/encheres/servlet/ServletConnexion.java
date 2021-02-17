ba9cab3afb1e2c348bd415f1f4305083702adc09
Camille <camille.boutin1@gmail.com>
2021-02-17 14:57:24 +0100

    14h57

--- src/fr/eni/encheres/servlet/ServletConnexion.java
+++ src/fr/eni/encheres/servlet/ServletConnexion.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class servletConnexion
  */
-@WebServlet("servletConnexion")
-public class servletConnexion extends HttpServlet {
+@WebServlet("/ServletConnexion")
+public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
