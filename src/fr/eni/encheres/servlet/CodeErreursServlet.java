0fb241168dab01b0f83a772f903cf8eff909912c
Camille <camille.boutin1@gmail.com>
2021-02-17 17:45:07 +0100

    17h45

---
+++ src/fr/eni/encheres/servlet/CodeErreursServlet.java
@@ -0,0 +1,8 @@
+package fr.eni.encheres.servlet;
+
+//Codes entre 30000 et 39999
+public abstract class CodeErreursServlet {
+
+	public static final int ERREUR_AUTHENTIFICATION= 30000;
+
+}
--- src/fr/eni/encheres/servlet/ServletConnexion.java
+++ src/fr/eni/encheres/servlet/ServletConnexion.java
@@ -2,6 +2,8 @@ package fr.eni.encheres.servlet;
 
 import java.io.IOException;
 import java.security.interfaces.RSAKey;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bll.UtilisateurManager;
@@ -38,20 +41,36 @@ public class ServletConnexion extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		HttpSession session = request.getSession();
 
 		String identifiant = request.getParameter("identifiant");
 		String motDePasse = request.getParameter("motdepasse");
+		
+		session.setAttribute(identifiant, motDePasse);
 		UtilisateurManager utilisateurManager = new UtilisateurManager();
-
+		RequestDispatcher rd=null;
+//pseudo et mot de passe correspondent-ils au pseudo mot de passe en BDD ?
 		try {
-			if (identifiant.equals(utilisateurManager.retournerUtilisateur(identifiant))&&motDePasse.equals(utilisateurManager.retournerUtilisateur(motDePasse))) {
-
+			if (identifiant.equals(utilisateurManager.retournerUtilisateur(identifiant))
+					&& motDePasse.equals(utilisateurManager.retournerUtilisateur(motDePasse))) {
+//				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+//	rd.forward(request, response);
+				rd= request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+				// TODO::: ajouter la liste des articles et enchères en mode détails et
+				// cliquables
 			}
 		} catch (BusinessException ex) {
-			// TODO Auto-generated catch block
 			ex.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodeErreursServlet.ERREUR_AUTHENTIFICATION);
+			request.setAttribute("erreur",ex.getListeErreurs());
+//			response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Test scunauthorized");
+
+
+			 rd = request.getRequestDispatcher("/WEB-INF/erreurAuthentification.jsp");
+//			rd.forward(request, response);
+
 		}
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
 		rd.forward(request, response);
 
 //doGet(request, response);
