a7bc8156067caea575f234fb67ecab6826204c1e
Camille <camille.boutin1@gmail.com>
2021-02-17 14:06:24 +0100

    commit 14h06

--- src/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -8,29 +8,25 @@ import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurManager {
-	// Doit disposer d'un accès à la DAL en passant par le DAOFactory => Création de la DAOFactory dans DAL
+	// Doit disposer d'un accès à la DAL en passant par le DAOFactory => Création de
+	// la DAOFactory dans DAL
 	private UtilisateurDAO daoUtilisateur;
-	
+
 	public UtilisateurManager() {
-daoUtilisateur=DAOFactory.getUtilisateurDAO();}
-	
-	
+		daoUtilisateur = DAOFactory.getUtilisateurDAO();
+	}
 
-	// Méthode pour obtenir la liste des utilisateurs présents en BDD
-	public List<Utilisateur> getListeUtilisateurs() throws BusinessException {
-		return this.daoUtilisateur.selectAll();
-		}
-	
+//	// Méthode pour obtenir la liste des utilisateurs présents en BDD
+//	public List<Utilisateur> getListeUtilisateurs() throws BusinessException {
+//		return this.daoUtilisateur.selectAll();
+//		}
 
-//public void verifierUtilisateur(String pseudo, String motDePasse,List<Utilisateur> daoUtilisateurs) {
-//	if(identifiant.length()<30 && identifiant.)
-//	
+public void verifierUtilisateur(String pseudo, String motDePasse,List<Utilisateur> daoUtilisateurs) {
+	if(identifiant.length()<30 && identifiant.)
 	
-
 	// Méthode pour obtenir un utilisateur de la BDD
 	public void ListeUtilisateur() {
 		daoUtilisateur = DAOFactory.getUtilisateurDAO();
 	}
-	
 
 }
