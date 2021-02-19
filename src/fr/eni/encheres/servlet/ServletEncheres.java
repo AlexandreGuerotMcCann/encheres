6b7928d2b3402a2e82cb079fff1c989d450ac516
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-19 09:47:50 +0100

    création des pages profil vente encheres...

---
+++ src/fr/eni/encheres/servlet/ServletEncheres.java
@@ -0,0 +1,32 @@
+package fr.eni.encheres.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEncheres
+ */
+@WebServlet("/ServletEncheres")
+public class ServletEncheres extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String ENCHERES="/WEB-INF/encheres.jsp";
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(ENCHERES).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/fr/eni/encheres/servlet/ServletMonProfil.java
@@ -0,0 +1,34 @@
+package fr.eni.encheres.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletMonProfil
+ */
+@WebServlet("/ServletMonProfil")
+public class ServletMonProfil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String PROFIL="/WEB-INF/profil.jsp";
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(ENCHERES).forward( request, response );
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
