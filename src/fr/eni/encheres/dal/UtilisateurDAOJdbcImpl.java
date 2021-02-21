b248b56c88f84715d3b1e7adc3ff2bbb290f37c8
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-21 12:08:00 +0100

    cast sur daojdbcimpl

--- src/fr/eni/encheres/dal/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/UtilisateurDAOJdbcImpl.java
@@ -55,7 +55,7 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 			System.out.println("aucun utilisateur");
 		}
 
-		return utilisateur;
+		return (List<Utilisateur>) utilisateur;
 	}		
 
 	@Override
