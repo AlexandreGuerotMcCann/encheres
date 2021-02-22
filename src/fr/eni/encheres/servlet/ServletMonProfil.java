6d3b91317b22de806ca49d00ef28d81296db5c40
Camille <camille.boutin1@gmail.com>
2021-02-22 16:24:55 +0100

    16h24

--- src/fr/eni/encheres/servlet/ServletMonProfil.java
+++ src/fr/eni/encheres/servlet/ServletMonProfil.java
@@ -1,11 +1,14 @@
 package fr.eni.encheres.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ServletMonProfil
@@ -20,6 +23,11 @@ public class ServletMonProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String identifiant = request.getParameter("identifiant");
+		String motDePasse = request.getParameter("motdepasse");
+		HttpSession session = request.getSession();
+		RequestDispatcher rd = null;
 		this.getServletContext().getRequestDispatcher(PROFIL).forward( request, response );
 
 	}
@@ -28,6 +36,8 @@ public class ServletMonProfil extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
 		doGet(request, response);
 		
 		
