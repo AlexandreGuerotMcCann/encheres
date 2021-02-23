95af1fcf8725e57b05ae3e48342db4a8113d2da1
Karlo <75167485+Karlow44@users.noreply.github.com>
2021-02-23 13:53:04 +0100

    article manager BLL

---
+++ src/fr/eni/encheres/bll/ArticleManager.java
@@ -0,0 +1,21 @@
+package fr.eni.encheres.bll;
+
+import fr.eni.encheres.dal.ArticleVenduDAO;
+import fr.eni.encheres.dal.DAOFactory;
+
+
+public class ArticleManager {
+
+	
+	private static final String INSTANCE = null;//
+	// Doit disposer d'un accès à la DAL en passant par le DAOFactory => Création de
+	// la DAOFactory dans DAL
+	private ArticleVenduDAO daoArticleVendu;
+	
+	public ArticleManager() {
+		daoArticleVendu = DAOFactory.getArticleVenduDAO();
+	
+		
+	}
+
+}
