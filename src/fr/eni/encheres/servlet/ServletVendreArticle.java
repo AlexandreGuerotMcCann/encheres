a079efb3818b4a2a1d6e2c901601318ee50ffd28
Karlo <75167485+Karlow44@users.noreply.github.com>
2021-02-23 22:25:46 +0100

    DO POST Servlet VendreArticle

--- src/fr/eni/encheres/servlet/ServletVendreArticle.java
+++ src/fr/eni/encheres/servlet/ServletVendreArticle.java
@@ -1,11 +1,21 @@
 package fr.eni.encheres.servlet;
 
 import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.ArticleManager;
+import fr.eni.encheres.bll.EncheresManager;
+import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class ServletVendreArticle
@@ -27,7 +37,29 @@ public class ServletVendreArticle extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String nomArticle = request.getParameter("nomArticle");
+		String description = request.getParameter("description");
+		String dateDebutEnchere = request.getParameter("dateDebutEnchere");
+		String dateFinEnchere = request.getParameter("dateFinEnchere");
+		String misAPrix = request.getParameter("miseAPrix");
+		int miseAPrix= Integer.parseInt(misAPrix);
+		String prixDepart = request.getParameter("prixVente");
+		int prixVente= Integer.parseInt(prixDepart);
+		String utilisateur = request.getParameter("noUtilisateur");
+		String categorie = request.getParameter("noCategorie");
+		
+		/** String DATE a Convertir au format DATE ou LocalDate ligne 42 et 43.
+		 * Modifier ensuite paramètre méthode ajoutArticleVendu ligne 58 **/
+		
+		RequestDispatcher rd = null;
+		
+		ArticleManager articleManager = new ArticleManager();
+		
+		ArticleVendu articleVendu= ArticleManager.ajoutArticleVendu(nomArticle, description, dateDebutEnchere, dateFinEnchere, miseAPrix, prixVente, utilisateur, categorie);
+	 
+
+		rd.forward(request, response);
+		
 	}
 
 }
