58522d43933ff0b3ceaeec336e1528943eff01d2
SandrineZ3 <79086955+SandrineZ3@users.noreply.github.com>
2021-02-24 18:07:35 +0100

    _

--- src/fr/eni/encheres/servlet/CodeErreursServlet.java
+++ src/fr/eni/encheres/servlet/CodeErreursServlet.java
@@ -3,7 +3,7 @@ package fr.eni.encheres.servlet;
 //Codes entre 30000 et 39999
 public abstract class CodeErreursServlet {
 
-	public static final int ERREUR_AUTHENTIFICATION= 30000;
+	public static final int ERREUR_AUTHENTIFICATION= 30000;  
 
 }
  
