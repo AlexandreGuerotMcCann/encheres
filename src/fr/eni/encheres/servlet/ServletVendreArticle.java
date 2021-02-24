08d7cb08479e235ee995e7a72724225e2cd2d12f
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-24 18:05:51 +0100

    ENFIN VENDRE ARTICLE

--- src/fr/eni/encheres/servlet/ServletVendreArticle.java
+++ src/fr/eni/encheres/servlet/ServletVendreArticle.java
@@ -1,7 +1,7 @@
 package fr.eni.encheres.servlet;
 
 import java.io.IOException;
-import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,13 +11,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bll.ArticleManager;
-import fr.eni.encheres.bll.EncheresManager;
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Categorie;
-import fr.eni.encheres.bo.Enchere;
 import fr.eni.encheres.bo.Utilisateur;
 
 /**
@@ -26,45 +23,49 @@ import fr.eni.encheres.bo.Utilisateur;
 @WebServlet("/ServletVendreArticle")
 public class ServletVendreArticle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final String NEWVENTE="/WEB-INF/vendreArticle.jsp";
- 
+	public static final String NEWVENTE = "/WEB-INF/vendreArticle.jsp";
+	public static final String MESVENTES = "/WEB-INF/mesVentes.jsp";
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher(NEWVENTE).forward( request, response );
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(NEWVENTE).forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nomArticle = request.getParameter("nomArticle");
-		String description = request.getParameter("description");
-		Date dateDebutEnchere = new SimpleDateFormat("yyyy-MM-dd").parse(getInitParameter("dateDebutEnchere"));
-		Date dateFinEnchere = new SimpleDateFormat("yyyy-MM-dd").parse(getInitParameter("dateFinEnchere"));
-		String misAPrix = request.getParameter("miseAPrix");
-		int miseAPrix= Integer.parseInt(misAPrix);
-		String prixDepart = request.getParameter("prixVente");
-		int prixVente= Integer.parseInt(prixDepart);
-		Utilisateur noUtilisateur = Utilisateur.parseInt("noUtilisateur");
-		Categorie noCategorie = Categorie.parseInt("noCategorie");
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String nomArticle = request.getParameter("nomArticle");
+			String description = request.getParameter("description");
+			Date dateDebutEnchere = new SimpleDateFormat("dd-MM-yyyy").parse(getInitParameter("dateDebutEnchere"));
+			Date dateFinEnchere = new SimpleDateFormat("dd-MM-yyyy").parse(getInitParameter("dateFinEnchere"));
+			String misAPrix = request.getParameter("miseAPrix");
+			int miseAPrix = Integer.parseInt(misAPrix);
+			String prixDepart = request.getParameter("prixVente");
+			int prixVente = Integer.parseInt(prixDepart);
+			Utilisateur noUtilisateur = Utilisateur.parseInt("noUtilisateur");
+			Categorie noCategorie = Categorie.parseInt("noCategorie");
 
-		
-		/** String DATE a Convertir au format DATE ou LocalDate ligne 42 et 43.
-		 * Modifier ensuite paramètre méthode ajoutArticleVendu ligne 58 **/
-		
-		RequestDispatcher rd = null;
-		
-		ArticleManager articleManager = new ArticleManager();
-		
-		ArticleVendu articleVendu= ArticleManager.ajoutArticleVendu(nomArticle, description, dateDebutEnchere, dateFinEnchere, miseAPrix, prixVente, noUtilisateur, noCategorie);
-	 
+			ArticleManager articleManager = new ArticleManager();
 
-		rd.forward(request, response);
-		
+			ArticleVendu articleVendu = articleManager.ajoutArticleVendu(nomArticle, description, dateDebutEnchere,
+					dateFinEnchere, miseAPrix, prixVente, noUtilisateur, noCategorie);
+			RequestDispatcher rd = null;
+			this.getServletContext().getRequestDispatcher(MESVENTES).forward(request, response);
+			rd.forward(request, response);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
