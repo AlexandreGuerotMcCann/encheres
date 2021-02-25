19b3c5a2b23fa24ba48d4ab0a744b75ca0192c9e
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-25 23:29:36 +0100

    test2

--- src/fr/eni/encheres/servlet/ServletAccueil.java
+++ src/fr/eni/encheres/servlet/ServletAccueil.java
@@ -64,7 +64,7 @@ public class ServletAccueil extends HttpServlet {
 //		{
  //   		this.getServletContext().getRequestDispatcher("/ServletAccueilDeconnecte").forward(request, response); // Sinon on go a l'accueil en mode déconnecté
 //		}   //(sans display du bouton déconnexion)
-//		
+//		    
 //		this.getServletContext().getRequestDispatcher(ACCUEIL).forward( request, response ); // OK A GARDER
 	}
 
