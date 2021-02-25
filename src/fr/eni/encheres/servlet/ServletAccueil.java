3bae2147e03ff1cadc5e9e3c8e6ae8d17dca8311
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-25 22:11:52 +0100

    redirection + accueil fait

--- src/fr/eni/encheres/servlet/ServletAccueil.java
+++ src/fr/eni/encheres/servlet/ServletAccueil.java
@@ -26,7 +26,7 @@ public class ServletAccueil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)    
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(); //
+		this.getServletContext().getRequestDispatcher(ACCUEIL).forward( request, response );
 		
 //		if (session.getAttribute("no_utilisateur")!=null) // Si utilisateur connecté  
 //		{
