e8f100e3f6267084ff79b124e941bfde55b60a20
Camille <camille.boutin1@gmail.com>
2021-02-25 23:24:31 +0100

    23h24

--- src/fr/eni/encheres/servlet/ServletAccueil.java
+++ src/fr/eni/encheres/servlet/ServletAccueil.java
@@ -31,7 +31,6 @@ public class ServletAccueil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)    
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher(ACCUEIL).forward( request, response );
 		
 		RequestDispatcher rd = null;
 		ArticleManager articleManager = new ArticleManager();
@@ -84,7 +83,7 @@ public class ServletAccueil extends HttpServlet {
 		request.setAttribute("motdepasse", motdepasse);
 		pseudo = (String)session.getAttribute ( "pseudo" );
 		
-
+		this.getServletContext().getRequestDispatcher(ACCUEIL).forward( request, response );
 
 	}
 
