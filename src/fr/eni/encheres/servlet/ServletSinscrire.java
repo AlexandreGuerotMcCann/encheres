429b99e70ad4198adf1dfb2c87b4bccc03bc01e3
SandrineZ3 <79086955+SandrineZ3@users.noreply.github.com>
2021-02-26 12:51:43 +0100

    __

--- src/fr/eni/encheres/servlet/ServletSinscrire.java
+++ src/fr/eni/encheres/servlet/ServletSinscrire.java
@@ -1,26 +1,20 @@
 package fr.eni.encheres.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bll.UtilisateurManager;
 import fr.eni.encheres.bo.Utilisateur;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.eclipse.jdt.internal.compiler.ast.ThrowStatement;
 
 /**
  * Servlet implementation class ServletSinscrire
