c5387781319a50441556f8e033ce808cf99c35b7
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-26 11:21:34 +0100

    categorieManager

---
+++ src/fr/eni/encheres/bll/CategorieManager.java
@@ -0,0 +1,52 @@
+package fr.eni.encheres.bll;
+
+import java.util.List;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.dal.CategoriesDAO;
+import fr.eni.encheres.dal.DAOFactory;
+
+public class CategorieManager {
+
+	private CategoriesDAO daoCategories;
+
+	public CategorieManager() {
+		daoCategories = DAOFactory.getCategorieDAO();
+
+	}
+
+	public Categorie retournerCategorie(String libelle) throws BusinessException {
+		return daoCategories.selectByLibelle(libelle);
+	}
+
+	public Categorie retournerUtilisateurParId(int noCatgeorie) throws BusinessException {
+		return daoCategories.selectById(noCatgeorie);
+	}
+
+	public List<Categorie> ListeCategorie() throws BusinessException {
+		return daoCategories.selectAll();
+	}
+
+	public Categorie ajoutCategorie(String libelle, int noCategorie) throws BusinessException {
+
+		Categorie categorie = new Categorie();
+		categorie.setLibelle(libelle);
+		categorie.setNoCategorie(noCategorie);
+
+
+		this.daoCategories.ajoutCategorie(categorie);
+
+		return categorie;
+	}
+	
+	public void modifierCategorie(Categorie categorie) throws BusinessException {
+		
+		
+		this.daoCategories.modifierCategorie(categorie);
+	}
+
+	public void supprimerCategorie(int noCategories) throws BusinessException {
+		this.daoCategories.supprimerCategorie(noCategories);
+	}
+}
