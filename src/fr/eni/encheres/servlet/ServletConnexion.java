b8ebd27ad5010673bd86422dd48d68a2e023f9f7
rambaldi35 <79100457+rambaldi35@users.noreply.github.com>
2021-02-26 10:15:07 +0100

    sdqdsqd

--- src/fr/eni/encheres/servlet/ServletConnexion.java
+++ src/fr/eni/encheres/servlet/ServletConnexion.java
@@ -56,7 +56,7 @@ public class ServletConnexion extends HttpServlet {
 				session.setAttribute("userTest", utilisateur);
 //            	clé "utilisateur" pour l'expression Language page accueil
 				session.setAttribute("utilisateur", utilisateur);
-				rd = request.getRequestDispatcher("/WEB-INF/ServletAccueil");
+				rd = request.getRequestDispatcher("ServletAccueil");
 				rd.forward(request, response);
 			} else {
 				rd = request.getRequestDispatcher("/WEB-INF/erreurAuthentification.jsp");
